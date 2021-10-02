f03c5c1d2e60475f372a139ef18f9096a728b3ca
Marek Grudkowski <marek.grudkowski@intel.com>
2021-10-02 20:29:05 +0200

    Javascript autoformat

--- source/frontend/src/App.js
+++ source/frontend/src/App.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Home from './containers/Home';
-import Login from './containers/Login';
-import Profile from './containers/Profile';
-import Signup from './containers/Signup';
-import Activate from './containers/Activate';
-import ResetPassword from './containers/ResetPassword';
-import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
-import Courses from './containers/Courses';
+import Home from './containers/Home'
+import Login from './containers/Login'
+import Profile from './containers/Profile'
+import Signup from './containers/Signup'
+import Activate from './containers/Activate'
+import ResetPassword from './containers/ResetPassword'
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
+import Courses from './containers/Courses'
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from 'react-redux'
+import store from './store'
 
-import Layout from './hocs/Layout';
+import Layout from './hocs/Layout'
 
 const App = () => (
     <Provider store={store}>
         <Router>
             <Layout>
                 <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route exact path='/login' component={Login} />
-                    <Route exact path='/profile' component={Profile} />
-                    <Route exact path='/signup' component={Signup} />
-                    <Route exact path='/reset-password' component={ResetPassword} />
-                    <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
-                    <Route exact path='/activate/:uid/:token' component={Activate} />
-                    <Route exact path='/courses' component={Courses} />
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route exact path="/signup" component={Signup} />
+                    <Route
+                        exact
+                        path="/reset-password"
+                        component={ResetPassword}
+                    />
+                    <Route
+                        exact
+                        path="/password/reset/confirm/:uid/:token"
+                        component={ResetPasswordConfirm}
+                    />
+                    <Route
+                        exact
+                        path="/activate/:uid/:token"
+                        component={Activate}
+                    />
+                    <Route exact path="/courses" component={Courses} />
                 </Switch>
             </Layout>
         </Router>
     </Provider>
-);
+)
 
-export default App;
+export default App
--- source/frontend/src/reducers/index.js
+++ source/frontend/src/reducers/index.js
@@ -1,10 +1,10 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import course from './course';
-import profile from './profile';
+import { combineReducers } from 'redux'
+import auth from './auth'
+import course from './course'
+import profile from './profile'
 
 export default combineReducers({
     auth,
     course,
-    profile
-});
+    profile,
+})
