c2435a727e720a50b92b8dcb38c324e2f9ec03ec
Marek Grudkowski <marek.grudkowski@intel.com>
2021-11-08 22:56:00 +0100

    Fix Style in Course

--- source/frontend/src/components/Home/HomeWelcome.js
+++ source/frontend/src/components/Home/HomeWelcome.js
@@ -10,18 +10,6 @@ const HomeWelcome = () => {
                     do realizowania kursów o różnej tematyce z wykorzystaniem
                     mechanizmów gamifikacji.
                 </p>
-                <p>
-                    Jesteś nauczycielem, który szuka ciekawego sposobu by
-                    zainteresować uczniów, wzbudzić w nich ciekawość, czy
-                    zwiększyć motywację? W takim razie ta strona jest właśnie
-                    dla Ciebie.
-                </p>
-                <p>
-                    Pozwala ona na utworzenie kursu o dowolnej tematyce, w
-                    którym uczestnicy nie będą rozwiązywać zadań, lecz
-                    przykładowo walczyć z potworami by rozwijać swojego
-                    bohatera.
-                </p>
             </div>
         </>
     )
