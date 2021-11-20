5885ace0a1a49de5297d01e5840b5d396da7f905
kacz98 <kacz@atinea.pl>
2021-11-20 21:07:49 +0100

    Fix courses page

---
+++ source/frontend/src/components/Courses/CourseLink.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Course.css'
+
+const CourseLink = ({ destination, text }) => {
+    return (
+        <div>
+            <Link className="course-title" to={destination}>
+                <h4 className="course-title">{text}</h4>
+            </Link>
+        </div>
+    )
+}
+
+export default CourseLink
