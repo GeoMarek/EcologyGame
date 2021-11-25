eb762e042a5cf426de9ecd14cb203e5034a9c8a8
kacz98 <kacz@atinea.pl>
2021-11-25 20:02:08 +0100

    fix style in hero page

--- source/frontend/src/components/Character/CharacterPresent.js
+++ source/frontend/src/components/Character/CharacterPresent.js
@@ -7,18 +7,14 @@ import './Character.css'
 const CharacterPresent = ({ character, edit_mode }) => {
     return (
         <div>
-            <h3 className="home-title">Witaj na stronie bohatera</h3>
-            <p>
-                Stoi przed Tobą wojownik {character.level} poziomu. <br />{' '}
-                Potężny i niepokonany {character.name}!
-            </p>
+            <h3 className="home-title">Oto twój bohater, {character.name}</h3>
             <div className="profile-image">
                 <CharacterImage is_alive={character.isAlive} />
-                <br />
-                <CommonButton text="Edytuj postać" on_click={edit_mode} />
             </div>
             <div className="profile-info">
                 <CharacterInfo character={character} />
+                <br />
+                <CommonButton text="Edytuj postać" on_click={edit_mode} />
             </div>
         </div>
     )
